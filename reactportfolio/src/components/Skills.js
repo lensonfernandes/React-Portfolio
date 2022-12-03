62ad46e3403275ea0fff7adea4ecfe72d5bf5fcb
lensonfernandes <lenson.fernand@gmail.com>
2022-12-03 11:38:16 +0530

    Add Images to Skills Section

--- reactportfolio/src/components/Skills.js
+++ reactportfolio/src/components/Skills.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import {Container, Row, Col} from "react-bootstrap"
+import reactImg from '../assets/img/reactImg.jpg'
+import htmlImg from '../assets/img/htmlImg.png'
+import cssImg from '../assets/img/cssImg.png'
 
 export default function Skills() {
   const responsive = {
@@ -31,21 +34,21 @@ return (
         <Col>
           <div className="skill-bx">
               <h2> Skills </h2>
-              <p> This is dummy text and just a placeholder</p>
+              <p> I use these Technology/Tools to make Pure Magic!!! </p>
               <Carousel responsive={responsive} infinite={true} className="skill-slider">
                 <div className="item">
-                  <img src={""} alt="image" />
-                  <h5> Web Development</h5>
+                  <img src={reactImg} alt="image" style={{borderRadius:'50%'}}/>
+                  <h5>React</h5>
                 </div>
 
                 <div className="item">
-                  <img src={""} alt="image" />
-                  <h5> Front End Engineer</h5>
+                  <img src={htmlImg} alt="image" style={{borderRadius:'50%'}} />
+                  <h5>HTML</h5>
                 </div>
 
                 <div className="item">
-                  <img src={""} alt="image" />
-                  <h5> React Developer</h5>
+                  <img src={cssImg} alt="image" style={{borderRadius:'50%'}} />
+                  <h5> CSS</h5>
                 </div>
               </Carousel>
           </div>
